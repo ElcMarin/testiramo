e27a45d338f3df12f993628016650733a9239502
ElcMarin <eliostante123@gmail.com>
2024-02-10 19:40:51 +0100

    images

---
+++ Models/appointment_type.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace maturitetna.Models
+{
+	public class appointment_typeEntity
+	{
+		[Key]
+		
+		public int id_appointment_type { get; set; }
+		public int duration { get; set; }
+		public int id_haircut { get; set; } 
+		public int id_hairdresser { get; set; } 
+	}
+}
+
---
+++ Models/hairdresserHaircut.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace maturitetna.Models
+{
+	public class HairdresserHaircutEntity
+	{
+		[Key]
+		public int id_hairdresserHaircut { get; set; }
+		public int id_hairdresser { get; set; }
+		public int id_haircut { get; set; }
+	}
+}
+
