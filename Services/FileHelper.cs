b6478dbdf2b4a692dad1cdf171168c2e7191218c
ElcMarin <eliostante123@gmail.com>
2024-02-29 14:39:48 +0100

    profile, pictures, pictures for hairdressrs when making an appointment

--- Services/FileHelper.cs
+++ Services/FileHelper.cs
@@ -5,9 +5,9 @@ public class FileHelper
     static public string GetProfilePicture( int id, char rights)
     {
         //kak nardim poizvedbo tule notr da vidim v kateri tabeli je pac po pravicah
-        if (System.IO.File.Exists("/Storage/ProfilePics/" + id + rights + ".png"))
+        if (System.IO.File.Exists("wwwroot/Storage/ProfilePics/" + id + "_" + rights + ".png"))
         {
-            return "/Storage/ProfilePics/"  + id + rights + ".png";
+            return "/Storage/ProfilePics/" + id + "_" + rights + ".png";
         }
         else
         {
